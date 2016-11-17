f93bf4b78af60d9f1fb78edfc2f232ed413e0c05
Logan Murdock <logtheman@gmail.com>
2016-11-16 16:39:37 -0800

    Added modal for viewing

--- app/models/attachement.rb
+++ app/models/attachement.rb
@@ -1,7 +1,7 @@
 class Attachement < ActiveRecord::Base
 	belongs_to :attachable, polymorphic: true	
 
-	has_attached_file :image, styles: { thumb: "64x64", med: "200x200", large: "400x400" }, 
+	has_attached_file :image, styles: { thumb: "64x64", med: "300x300", large: "600x600" }, 
 		default_url: ":rails_root/public/system/app/images/:style/missing.png",
         :url  => "/assets/images/posts/:basename.:extension",
         :path => ":rails_root/public/assets/images/posts/:basename.:extension"
