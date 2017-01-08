ebdaaa87efaa6838795eb7ee6a04cfcf19d052fb
Logan Murdock <logtheman@gmail.com>
2017-01-08 15:30:31 -0800

    Push ready file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  attr_accessor :image
   helper_method :current_user , :logged_in?
   before_filter :configure_permitted_parameters, if: :devise_controller?
   
@@ -16,10 +16,13 @@ class ApplicationController < ActionController::Base
       u.permit(:name, :username, :image,
         :email, :password, :password_confirmation)
     end
-    devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :username, :image,
-        :email, :password, :password_confirmation, :current_password)
-    end
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username, :image, 
+        :email, :password, :password_confirmation, :current_password])
+    # devise_parameter_sanitizer.permit(:account_update) do |u|
+    #  u.permit(:name, :username, :image,
+    #    :email, :password, :password_confirmation, :current_password)
+    # end
+
   end
 
   def logged_in?
